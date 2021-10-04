2f9802cca21588bab43e84dd032625ba52440dd7
Ben Spotswood <spotswoodb@gmail.com>
2021-10-03 21:13:25 -0500

    Add create method in venues controller and venue params

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -9,4 +9,18 @@ class VenuesController < ApplicationController
     #     venue 
     # end
 
+    def create 
+        venue = Venue.new(venue_params)
+        if venue.save 
+            render json: venue
+        else
+            render json: {error: "Couldnt be saved"}
+        end
+    end
+
+private
+
+    def venue_params
+        params.require(:venue).permit(:name, :address, :all_events)
+    end    
 end

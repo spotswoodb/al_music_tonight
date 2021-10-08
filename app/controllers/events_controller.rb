c4d38c073af4bd7342296d42fee0c48be36b9e16
Ben Spotswood <spotswoodb@gmail.com>
2021-10-08 16:20:27 -0500

    add create and event_params in event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,19 @@ class EventsController < ApplicationController
         events = Event.all
         render json: EventSerializer.new(events, {include: [:venue]})
     end
+
+    def create 
+        event = Event.new(event_params)
+        if event.save
+            render json: event
+        else
+            render json: {error: "Couldn't be saved"}
+        end
+    end
+
+
+private
+    def event_params
+        params.require(:event).permit(:name, :date, :time, :image, :venue_id)
+    end
 end

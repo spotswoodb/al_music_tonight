58f6bdb426c5834987d33b89d77ef41f9a879743
Ben Spotswood <spotswoodb@gmail.com>
2021-10-12 00:38:59 -0500

    add show method in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,10 @@ class EventsController < ApplicationController
         render json: EventSerializer.new(events, {include: [:venue]})
     end
 
+    def show
+        render json: event 
+    end
+
     def create 
         event = Event.new(event_params)
         if event.save
@@ -16,6 +20,7 @@ class EventsController < ApplicationController
 
 
 private
+
     def event_params
         params.require(:event).permit(:name, :date, :time, :image, :venue_id)
     end
